0b473e12c3554f36c37576932ea9b1d290aa7623
Franpall <98850575+Franpall@users.noreply.github.com>
2025-02-17 16:00:21 -0400

    Script principal para ejecutar la app con FLASK

---
+++ flaskr/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def iniciarSesion():
+    return render_template('auth/login.html')
+
+@app.route('/register')
+def registrarse():
+    return render_template('auth/register.html')
+
+@app.route('/carrito')
+def verCarrito():
+    return render_template('carrito.html')
+
+@app.route('/producto')
+def verProducto():
+    return render_template('producto.html')
