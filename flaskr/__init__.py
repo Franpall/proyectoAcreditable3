dafbf4f68b099aaa4e24e298a45c9e9a958c4798
DaniVzla <108209944+DaniVzla@users.noreply.github.com>
2025-02-19 10:51:31 -0400

    Update para pasar los productos a index

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
+from flaskr.dbConexion import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', , productos=mostrar_productos())
 
 @app.route('/login')
 def iniciarSesion():
