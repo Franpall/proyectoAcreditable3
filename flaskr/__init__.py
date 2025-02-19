fae4f73e92ca8f1b7aca2314759551bbf681fce1
DaniVzla <108209944+DaniVzla@users.noreply.github.com>
2025-02-19 10:53:35 -0400

    bugfix xd

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', , productos=mostrar_productos())
+    return render_template('index.html', productos=mostrar_productos())
 
 @app.route('/login')
 def iniciarSesion():
