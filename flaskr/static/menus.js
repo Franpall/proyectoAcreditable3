e217e0213c774ab6f645f6515b5b80355eb0b188
Franpall <98850575+Franpall@users.noreply.github.com>
2025-02-22 12:48:25 -0400

    El menú se cierra automáticamente

--- flaskr/static/menus.js
+++ flaskr/static/menus.js
@@ -1,6 +1,15 @@
 const menuButton = document.getElementById("user_btn");
 menuButton.onclick = cambiarEstadoVisible;
 
+const header = document.getElementsByTagName('header')[0];
+let headerTop = header.offsetTop;
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > headerTop) {
+    ocultarMenu()
+  }
+});
+
 function cambiarEstadoVisible(){
     const menuUser = document.getElementById("float_menu");
     if (menuUser.className == "hint"){
@@ -9,4 +18,11 @@ function cambiarEstadoVisible(){
     else{
         menuUser.className = "hint"
     }
+}
+
+function ocultarMenu(){
+  const menuUser = document.getElementById("float_menu");
+  if (menuUser.className != "hint"){
+    menuUser.className = "hint"
+  } 
 }
