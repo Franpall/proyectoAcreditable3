0f3cae77b705d5a7dbea1dd59e17c8daebc50e3c
DaniVzla <108209944+DaniVzla@users.noreply.github.com>
2025-03-05 15:57:37 -0400

    modelo del producto

--- flaskr/models.py
+++ flaskr/models.py
@@ -3,3 +3,10 @@ class Usuario:
         self.id = id
         self.nombre = nombre
         self.correo = correo
+
+class Producto:
+    def __init__(self, marca, modelo, imagen, precio):
+        self.marca = marca
+        self.modelo = modelo
+        self.imagen = imagen
+        self.precio = precio
