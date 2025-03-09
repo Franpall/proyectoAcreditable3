34681f87d6605931f25efec177f958f777c8b0d3
KeiverCalderon <136232522+KeiverCalderon@users.noreply.github.com>
2025-03-08 22:33:13 -0400

    Agregada logica para subir imagenes
    Y mejorada estructura del codigo en general

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,26 @@ import os
 app = Flask(__name__)
 sesion = False
 
+# Creación de carpeta para subir las imagenes
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
+
+# Crear carpeta uploads si no existe
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+    
+@app.route('/')
+def index():
+    categorias = obtener_categorias()
+
+    notificacion = request.args.get('notificacion', False)
+    actionError = request.args.get('actionError', False)
+    actionOK = request.args.get('actionOK', False)
+
+    return render_template('index.html', productos=mostrar_productos(), productos_recomendados=mostrar_productos_recomendados(), sesion=sesion, 
+        categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK
+    )
+
 @app.route('/login')
 def iniciarSesion():
     notificacion = request.args.get('notificacion', False)
@@ -37,10 +57,11 @@ def adminDashboard():
 @app.route('/productos')
 def adminProductos():
     categorias = verCategorias()
+    productos = mostrar_productos_admin()
     notificacion = request.args.get('notificacion', False)
     actionError = request.args.get('actionError', False)
     actionOK = request.args.get('actionOK', False)
-    return render_template('admin/productos.html', productos=mostrar_productos_admin(), categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK)
+    return render_template('admin/productos.html', productos=productos, categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK)
 
 @app.route('/clientes')
 def adminClientes():
@@ -63,15 +84,50 @@ def adminVentas():
 def adminUpdateUI():
     return render_template('admin/editarProducto.html')
 
-#Logica para el registro de productos
+
+# Lógica para agregar categorias
+@app.route('/categorias', methods=['GET', 'POST'])
+def adminCategorias():
+    notificacion = request.args.get('notificacion', False)
+    actionError = request.args.get('actionError', False)
+    actionOK = request.args.get('actionOK', False)
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        imagen = request.files['imagen']
+
+        if imagen:
+            filename = imagen.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            imagen.save(filepath)
+            agregar_categoria(nombre, filename)
+            return redirect(url_for('adminCategorias', actionOK=True, notificacion="Se Agregó la categoría"))
+    
+    categorias = obtener_categorias()
+    
+    return render_template('admin/categorias.html', categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK)
+
+# Eliminar Categorias
+@app.route('/delete_categoria/<int:id_categoria>')
+def eliminarCategoria(id_categoria):
+    eliminar_categoria(id_categoria)
+
+    return redirect(url_for('adminCategorias', actionOK=True, notificacion="Se eliminó la categoría"))
+
+
+# Lógica para el registro de productos
 @app.route('/registrarProductos', methods=('GET', 'POST'))
 def registrarProductos():
+    notificacion = request.args.get('notificacion', False)
+    actionError = request.args.get('actionError', False)
+    actionOK = request.args.get('actionOK', False)
+    
     if request.method == 'POST':
         marca = request.form['marca']
         modelo = request.form['modelo']
         descripcion = request.form['descripcion']
         categoria = request.form['categoria']
-        imagen = request.form['imagen']
+        imagen = request.files['imagen']
         precio = request.form['precio']
         stock = request.form['stock']
         try:
@@ -84,10 +140,22 @@ def registrarProductos():
 
         id_categoria = obtener_id_categoria(categoria)
 
-        resultado = agregar_producto(marca, modelo, descripcion, id_categoria, imagen, precio, stock, recomendado)
-
-        if resultado:
+        if imagen:
+            filename = imagen.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            imagen.save(filepath)
+            agregar_producto(marca, modelo, descripcion, id_categoria, filename, precio, stock, recomendado)
             return redirect(url_for('adminProductos', actionOK=True, notificacion="Producto Registrado con Éxito"))
+        
+    return render_template('admin/productos.html', notificacion=notificacion, actionError=actionError, actionOK=actionOK)
+
+# Eliminar Productos
+@app.route('/delete_producto/<int:id_producto>')
+def eliminarProducto(id_producto):
+    eliminar_producto(id_producto)
+
+    return redirect(url_for('adminProductos', actionOK=True, notificacion="Se eliminó el producto"))
+
 
 # Lógica para el registro de clientes
 @app.route('/registerSolicitud', methods=('GET', 'POST'))
@@ -151,50 +219,3 @@ def eliminarAdmin(id_admin):
     eliminar_admin(id_admin)
     return redirect(url_for('adminAdmins', actionOK=True, notificacion="Administrador Eliminado con exito"))
 
-
-# Lógica para CATEGORIAS
-
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
-
-# Crear carpeta uploads si no existe
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
-    
-@app.route('/')
-def index():
-    categorias = obtener_categorias()
-
-    notificacion = request.args.get('notificacion', False)
-    actionError = request.args.get('actionError', False)
-    actionOK = request.args.get('actionOK', False)
-
-    return render_template('index.html', productos=mostrar_productos(), productos_recomendados=mostrar_productos_recomendados(), sesion=sesion, 
-        categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK
-    )
-    
-@app.route('/categorias', methods=['GET', 'POST'])
-def adminCategorias():
-    notificacion = request.args.get('notificacion', False)
-    actionError = request.args.get('actionError', False)
-    actionOK = request.args.get('actionOK', False)
-
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        imagen = request.files['imagen']
-
-        if imagen:
-            filename = imagen.filename
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            imagen.save(filepath)
-            agregar_categoria(nombre, filename)
-            return redirect(url_for('adminCategorias', actionOK=True, notificacion="Se Agregó la categoría"))
-    
-    categorias = obtener_categorias()
-    
-    return render_template('admin/categorias.html', categorias=categorias, notificacion=notificacion, actionError=actionError, actionOK=actionOK)
-
-@app.route('/delete_categoria/<int:id_categoria>')
-def eliminarCategoria(id_categoria):
-    eliminar_categoria(id_categoria)
-
-    return redirect(url_for('adminCategorias', actionOK=True, notificacion="Se eliminó la categoría"))
