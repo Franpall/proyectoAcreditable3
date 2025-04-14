502971f910bbef86a97eea7b8e986f3586d69f9d
KeiverCalderon <kvrcalderon@gmail.com>
2025-04-14 19:01:59 -0400

    Incluir estados "Activo" e "Inactivos" para los clientes

--- franpalstore.sql
+++ franpalstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-04-2025 a las 09:19:38
+-- Tiempo de generación: 15-04-2025 a las 00:58:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -77,19 +77,20 @@ CREATE TABLE `usuario` (
   `usuario` varchar(20) NOT NULL,
   `correo` varchar(320) NOT NULL,
   `contraseña` blob NOT NULL,
-  `rol` enum('cliente','jefe','admin','supervisor') NOT NULL
+  `rol` enum('cliente','jefe','admin','supervisor') NOT NULL,
+  `activo` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `usuario`, `correo`, `contraseña`, `rol`) VALUES
-(1, 'jefe', 'jefe@gmail.com', 0x243262243132244d576132454234567134784c4a35727557732e75524f4962506a732f3550397a616a527a6a776f7a68342f776370706f4d7a744d57, 'jefe'),
-(2, 'franpal', 'skfranpal@gmail.com', 0x2432622431322473534c776a6a6d55587954373943504368634a4c41756b516468565556705765634b7668737855656f576142552e716973426d584f, 'cliente'),
-(3, 'keiver', 'keiver@gmail.com', 0x243262243132246b364a464f6b6b7937777073474c69366944354e4e4f4b6f5239394636525943763058707468544f2e45613642374e4f575156614b, 'cliente'),
-(4, 'admin', 'admin@gmail.com', 0x243262243132242e7257346d315256445a7875496463712e4378624a7567786475624a633441646477617852456f734d6d576b447478624e32594853, 'admin'),
-(5, 'supervisor', 'supervisor@gmail.com', 0x2432622431322468552f664633514766367551654848413330503773657168346330556335713134732e4839776a63486661726645614b49486c6269, 'supervisor');
+INSERT INTO `usuario` (`id`, `usuario`, `correo`, `contraseña`, `rol`, `activo`) VALUES
+(1, 'jefe', 'jefe@gmail.com', 0x243262243132244d576132454234567134784c4a35727557732e75524f4962506a732f3550397a616a527a6a776f7a68342f776370706f4d7a744d57, 'jefe', 1),
+(2, 'franpal', 'skfranpal@gmail.com', 0x2432622431322473534c776a6a6d55587954373943504368634a4c41756b516468565556705765634b7668737855656f576142552e716973426d584f, 'cliente', 1),
+(3, 'keiver', 'keiver@gmail.com', 0x243262243132246b364a464f6b6b7937777073474c69366944354e4e4f4b6f5239394636525943763058707468544f2e45613642374e4f575156614b, 'cliente', 1),
+(4, 'admin', 'admin@gmail.com', 0x243262243132242e7257346d315256445a7875496463712e4378624a7567786475624a633441646477617852456f734d6d576b447478624e32594853, 'admin', 1),
+(5, 'supervisor', 'supervisor@gmail.com', 0x2432622431322468552f664633514766367551654848413330503773657168346330556335713134732e4839776a63486661726645614b49486c6269, 'supervisor', 1);
 
 -- --------------------------------------------------------
 
@@ -152,19 +153,19 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -176,7 +177,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
